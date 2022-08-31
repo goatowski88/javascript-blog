2c59c5000a48420c27af43d9d91b00058eb9ea7f
Tomek Goatowski <tomaszkozlowski.tech@gmail.com>
2022-08-31 11:22:15 +0200

    Ad active article

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
 
-const activeArticles = document.querySelectorAll('.posts active');
+const activeArticles = document.querySelectorAll('.posts article.active');
 console.log('Find active post');
 
 for(let activeArticle of activeArticles){
@@ -38,13 +38,60 @@ for(let activeArticle of activeArticles){
 
   /* get 'href' attribute from the clicked link */
 
+  const href = clickedElement.getAttribute('href');
+
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(href);
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
+
 }
 
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector('.list.titles');
+  titleList.innerHTML = '';
+
+  /* for each article */
+
+  const allArticles = document.querySelectorAll('.post')
+  for(let article of allArticles){  
+    /* get the article id */
+    const id = article.getAttribute('id');
+
+    /* find the title element */
+
+    const title = article.querySelector('.post-title').innerHTML
+
+    /* create HTML of the link */
+
+    const linkHTML = '<li><a href="#'+ id +'"><span>'+ title +'</span></a></li>';
+          
+
+    /* insert link into titleList */
+    titleList.innerHTML = titleList.innerHTML + linkHTML;
+
+
+
+    }
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+}
+
+generateTitleLinks();
